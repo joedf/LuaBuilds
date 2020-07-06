ebf068768f8d8019f9d88890e5b105e9f287e533
Joe DF <joedf@live.ca>
2020-07-05 22:39:54 -0400

    build Lua v5.4.0 (release) and srlua-102

---
+++ PrivateBuild/src/lua-5.4.0/Lua_WinRes.bat
@@ -0,0 +1,20 @@
+@echo off
+rem by Joe DF
+rem This was tested with 'mingw' as the build platform
+rem After running the makefile,
+rem run this file to add the resources to lua.exe, luac.exe and lua52.dll
+rem Outputs to 'bin' folder
+cd src
+mkdir bin >nul 2>&1
+
+windres --output-format=coff --target=pe-i386 WinResource\lua.rc lua.rc.o
+windres --output-format=coff --target=pe-i386 WinResource\luac.rc luac.rc.o
+windres --output-format=coff --target=pe-i386 WinResource\dll.rc dll.rc.o
+
+gcc -m32 -shared -o bin\lua54.dll dll.rc.o lapi.o lcode.o lctype.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o lmem.o lobject.o lopcodes.o lparser.o lstate.o lstring.o ltable.o ltm.o lundump.o lvm.o lzio.o lauxlib.o lbaselib.o lcorolib.o ldblib.o liolib.o lmathlib.o loadlib.o loslib.o lstrlib.o ltablib.o lutf8lib.o linit.o
+
+strip --strip-unneeded lua54.dll
+gcc -m32 -o bin\lua.exe -s  lua.o lua.rc.o lua54.dll -lm
+gcc -m32 -o bin\luac.exe   luac.o luac.rc.o liblua.a -lm
+
+del WinResource\*.o >nul 2>&1
