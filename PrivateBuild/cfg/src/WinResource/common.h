1ba7d183e481c789d73b2ab2e53c647723ef4306
Joe DF <3848219+joedf@users.noreply.github.com>
2025-12-03 10:48:05 -0500

    update rc files to use common.h and fix mistakes

---
+++ PrivateBuild/cfg/src/WinResource/common.h
@@ -0,0 +1,7 @@
+#define LUA_VERSION 5,5,0,0
+#define LUA_VERSION_STR "5.5.0-rc2"
+#define G_COMMENTS "www.lua.org"
+#define G_COMPANYNAME "Lua.org"
+#define G_LEGALCOPYRIGHT "Copyright (C) 1994-2025 Lua.org, PUC-Rio."
+#define G_PRODUCTNAME "Lua - The Programming Language"
+#define G_PRIVATEBUILD "Build by Joe DF (joedf.github.io)"
