f83e3b4167801cfeef8cf94a63833ede17467b61
Joe DF <3848219+joedf@users.noreply.github.com>
2025-12-22 15:05:00 -0500

    build lua + srlua v5.5.0 + lpeg v1.1.0

--- PrivateBuild/src/srlua/common.h
+++ PrivateBuild/src/srlua/common.h
@@ -1,5 +1,5 @@
 #define LUA_VERSION 5,5,0,0
-#define LUA_VERSION_STR "5.5.0-rc4"
+#define LUA_VERSION_STR "5.5.0"
 #define G_COMMENTS "www.lua.org"
 #define G_COMPANYNAME "Lua.org"
 #define G_LEGALCOPYRIGHT "Copyright (C) 1994-2025 Lua.org, PUC-Rio."
