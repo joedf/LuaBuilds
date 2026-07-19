12e37d89be504a77525785b72d9af038c11aa48b
Joe DF <3848219+joedf@users.noreply.github.com>
2026-07-19 13:11:54 -0400

    build lua + srlua v5.5.1-rc1

--- PrivateBuild/cfg-srlua/common.h
+++ PrivateBuild/cfg-srlua/common.h
@@ -1,7 +1,7 @@
-#define LUA_VERSION 5,5,0,0
-#define LUA_VERSION_STR "5.5.0"
+#define LUA_VERSION 5,5,1,0
+#define LUA_VERSION_STR "5.5.1"
 #define G_COMMENTS "www.lua.org"
 #define G_COMPANYNAME "Lua.org"
-#define G_LEGALCOPYRIGHT "Copyright (C) 1994-2025 Lua.org, PUC-Rio."
+#define G_LEGALCOPYRIGHT "Copyright (C) 1994-2026 Lua.org, PUC-Rio."
 #define G_PRODUCTNAME "Lua - The Programming Language"
 #define G_PRIVATEBUILD "Build by Joe DF (joedf.github.io)"
